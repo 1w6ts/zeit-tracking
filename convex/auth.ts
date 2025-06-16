f862eab6be23c7e50ff2a6b843f489cf7348bfcb
1w6ts <xype.discord@gmail.com>
2025-06-16 19:45:45 +0200

    feat(auth): implement auth using convex and better-auth

---
+++ convex/auth.ts
@@ -0,0 +1,59 @@
+import {
+  BetterAuth,
+  convexAdapter,
+  type AuthFunctions,
+  type PublicAuthFunctions,
+} from "@convex-dev/better-auth";
+import { convex } from "@convex-dev/better-auth/plugins";
+import { betterAuth } from "better-auth";
+import { api, components, internal } from "./_generated/api";
+import { query, type GenericCtx } from "./_generated/server";
+import type { Id, DataModel } from "./_generated/dataModel";
+
+const authFunctions: AuthFunctions = internal.auth;
+const publicAuthFunctions: PublicAuthFunctions = api.auth;
+
+export const betterAuthComponent = new BetterAuth(components.betterAuth, {
+  authFunctions,
+  publicAuthFunctions,
+});
+
+export const createAuth = (ctx: GenericCtx) =>
+  betterAuth({
+    baseURL: "http://localhost:3000",
+    database: convexAdapter(ctx, betterAuthComponent),
+
+    emailAndPassword: {
+      enabled: true,
+    },
+    plugins: [convex()],
+  });
+
+export const {
+  createUser,
+  updateUser,
+  deleteUser,
+  createSession,
+  isAuthenticated,
+} = betterAuthComponent.createAuthFunctions<DataModel>({
+  onCreateUser: async (ctx, user) => {
+    return ctx.db.insert("users", {});
+  },
+  onDeleteUser: async (ctx, userid) => {
+    await ctx.db.delete(userid as Id<"users">);
+  },
+});
+
+export const getCurrentUser = query({
+  args: {},
+  handler: async (ctx) => {
+    const userMetadata = await betterAuthComponent.getAuthUser(ctx);
+    if (!userMetadata) return null;
+
+    const user = await ctx.db.get(userMetadata.userId as Id<"users">);
+    return {
+      ...user,
+      ...userMetadata,
+    };
+  },
+});
