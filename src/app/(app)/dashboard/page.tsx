9e3d674cb38b37a80aeac46f7da31c39d16a5002
1w6ts <xype.discord@gmail.com>
2025-06-16 23:15:48 +0200

    feat(app): implement session checking

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,18 @@
-export default function DashboardPage() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session?.session) {
+    redirect("/login");
+  }
   return (
     <div>
-      <h1>Welcome Back</h1>
+      <h1 className="text-4xl tracking-tighter">
+        Welcome Back, {session?.user.name}
+      </h1>
     </div>
   );
 }
